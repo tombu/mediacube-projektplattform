5a0d8a74e47aa37f1c94ca0c73f871e3e87bcbe5
Thomas Buchöster <tbuchoester.mmt-b2009@fh-salzburg.ac.at>
2010-12-26 15:13:36 +0100

    naked rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gitrepo::Application.config.secret_token = '85928dda106ee98efc40aa2b4a598b432a1eee8dce0fe655bf495046f2b78a080d9a679d9b70d623ae56e76af2f5106d516404b3dff8d9b528d64404d6e99681'
