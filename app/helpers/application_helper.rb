061e727fc0209fe91b0f23523eafbc5e73ddb628
Thomas Buchöster <tbuchoester.mmt-b2009@fh-salzburg.ac.at>
2011-01-07 13:32:25 +0100

    upload modified

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,7 +5,7 @@ module ApplicationHelper
 #    else
       default_url = "#{root_url}images/default_user.jpg"
       gravatar_id = Digest::MD5.hexdigest(user.mail.downcase)
-      "http://gravatar.com/avatar/#{gravatar_id}.png?s=48&d=#{CGI.escape(default_url)}"
+      "http://gravatar.com/avatar/#{gravatar_id}.png?s=40&d=#{CGI.escape(default_url)}"
 #    end
   end
 end
