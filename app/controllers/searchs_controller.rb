444a2b86f5a5b8f909329eefea3df6674ac52366
Thomas Buchöster <tbuchoester.mmt-b2009@fh-salzburg.ac.at>
2011-01-08 22:19:16 +0100

    conflicts solved + merge

--- app/controllers/searchs_controller.rb
+++ app/controllers/searchs_controller.rb
@@ -1,29 +1,35 @@
 class SearchsController < ApplicationController
   def index
     @cat = params[:category]
-    if params[:search].empty?
+    params[:search].lstrip!
+    
+    if params[:search].empty? || params[:search] == 'Suche nach'
       redirect_to :root
     else
-      @results = search(params).paginate(:page => params[:page], :per_page => 10)
+      @results = search params
+      @resultscount = @results.count
+      @results = @results.paginate :page => params[:page], :per_page => 30
     end
   end
   
   def search search
     q = search[:search]
     case search[:category]
-      when 'projects'
+      when 'Projekte'
         Project.find :all, :conditions => ["title LIKE ? or description LIKE ?", "%#{q}%", "%#{q}%"]
-      when 'jobs'
+      when 'Jobs'
         @open_projects = %w()
         @all_jobs = Job.find :all, :conditions => ["name LIKE ?", "%#{q}%"]
         @all_jobs.each do |job|
-          if !job.project.roles.include? job.role 
-            @open_projects << job.project 
+          if !job.project.roles.include? job.role
+            if !@open_projects.include? job.project
+              @open_projects << job.project
+            end
           end
         end
         @open_projects
-      when 'users'
-        Project.find :all
+      when 'User'
+        User.find :all, :conditions => ["name LIKE ? or mail LIKE ?", "%#{q}%", "%#{q}%"]
       else redirect_to :root
     end
   end
