73f12021e5238b81613e03cad9693d4e090eadf1
Thomas Buchöster <tbuchoester.mmt-b2009@fh-salzburg.ac.at>
2011-03-01 01:35:35 +0100

    edited seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,4 @@
-Category.create([
+﻿Category.create([
   {:label=>'Kunst'},
   {:label=>'Fotografie'},
   {:label=>'Film'},
@@ -16,13 +16,13 @@ Category.create([
   {:label=>'Sozial'}
 ])
 
-Texttemplates.create([
+Texttemplate.create([
   {:key=>'media_new',:text=>'#user hat neue Medien hochgeladen.'},
   {:key=>'project_edit',:text=>'Das Projekt wurde editiert.'},
   {:key=>'project_status_edit',:text=>'Der <b>Projektstatus</b> wurde auf <span>#status</span> gesetzt.'},
   {:key=>'project_current_stage_edit',:text=>'Das Projekt befindet sich jetzt in der Stage <span>#stage</span>.'},
   {:key=>'job_new',:text=>'Es wurden neue Jobs ausgeschrieben:<br /><span>#jobs</span>'},
-  {:key=>'job_delete',:text=>'Folgende Jobs sind nicht mehr verf�gbar:<br /><span>#jobs</span>'},
+  {:key=>'job_delete',:text=>'Folgende Jobs sind nicht mehr verfügbar:<br /><span>#jobs</span>'},
   {:key=>'team_new',:text=>'ist ein neues Teammitglied.'},
   {:key=>'team_delete',:text=>'hat das Team verlassen.'}
 ])
