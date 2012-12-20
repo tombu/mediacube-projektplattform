d30551eda276730f4b93d40c53283d35aa1b304f
Thomas Buchöster <thomas.buchoester@gmail.com>
2012-12-20 18:27:48 +0100

    Introduces configuration file for further settings

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Projektplattform::Application.config.secret_token = 'dfa2fee59314ad3edcc2c0a43b691a91a30c9b18aa3a2f483077e8794bdca148cd7195524a66059ce9a2dd7dc23e788585e54e62ef0f30a4f083d638f29d9555'
+Projektplattform::Application.config.secret_token = ENV['SECRET_TOKEN']
